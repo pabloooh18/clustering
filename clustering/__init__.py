0f255fd590b22c8ef54d2460ebd05ae56af3034d
Arturo CURIEL <curieldaz@gmail.com>
2017-11-16 00:40:48 -0300

    Changes in structure of code

--- clustering/__init__.py
+++ clustering/__init__.py
@@ -1,34 +1,38 @@
 import duc
 
 data_folder_original = "../res/original"
-data_folder_sum10 = "../res/sum10"
-data_folder_sum100 = "../res/sum100"
+data_folder_summ10 = "../res/summ10"
+data_folder_summ100 = "../res/summ100"
+
+output_folder_original = "../output/original"
+output_folder_summ10 = "../output/summ10"
+output_folder_summ100 = "../output/summ100"
 
 # Text dumps
-out_documents_file = "../res/rouge_docs.txt"
-out_rouge_space_file = "../res/rouge_space.txt"
-out_vectorized_documents_file = "../res/rouge_vect_docs.txt"
-out_centroids_file = "../res/rouge_centroids.txt"
+out_documents_file = "/rouge_docs.txt"
+out_rouge_space_file = "/rouge_space.txt"
+out_vectorized_documents_file = "/rouge_vect_docs.txt"
+out_centroids_file = "/rouge_centroids.txt"
 
 #Pickled dumps
-p_documents_file = "../res/rouge_docs.p"
-p_rouge_space_file = "../res/rouge_space.p"
-p_vectorized_documents_file = "../res/rouge_vect_docs.p"
-p_centroids_file = "../res/rouge_centroids.p"
+p_documents_file = "/rouge_docs.p"
+p_rouge_space_file = "/rouge_space.p"
+p_vectorized_documents_file = "/rouge_vect_docs.p"
+p_centroids_file = "/rouge_centroids.p"
 
 # Rouge centroids
 documents = duc.get_rouge_document_clusters(data_folder_original)
-duc.dump(documents, out_documents_file)
-duc.pickle_dump(documents, p_documents_file)
+duc.dump(documents, output_folder_original + out_documents_file)
+duc.pickle_dump(documents, output_folder_original + p_documents_file)
 
 rouge_space = duc.get_vector_space_from_clusters(documents)
-duc.dump(rouge_space, out_rouge_space_file)
-duc.pickle_dump(rouge_space, p_rouge_space_file)
+duc.dump(rouge_space, output_folder_original + out_rouge_space_file)
+duc.pickle_dump(rouge_space, output_folder_original +  p_rouge_space_file)
 
 vectorized_documents = duc.convert_to_vectors(documents, rouge_space)
-duc.dump(vectorized_documents, out_vectorized_documents_file)
-duc.pickle_dump(vectorized_documents, p_vectorized_documents_file)
+duc.dump(vectorized_documents, output_folder_original + out_vectorized_documents_file)
+duc.pickle_dump(vectorized_documents, output_folder_original + p_vectorized_documents_file)
 
 centroids = duc.get_cluster_centroids(vectorized_documents)
-duc.dump(centroids, out_centroids_file)
-duc.pickle_dump(centroids, p_centroids_file)
+duc.dump(centroids, output_folder_original + out_centroids_file)
+duc.pickle_dump(centroids, output_folder_original + p_centroids_file)
