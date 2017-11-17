b0a16452a6ae7e79c43ce54a05ed495c2fe84820
Arturo CURIEL <curieldaz@gmail.com>
2017-11-17 19:08:33 -0300

    Some error metrics

--- clustering/error_metrics.py
+++ clustering/error_metrics.py
@@ -34,29 +34,52 @@ def print_confusion_matrix(matrix):
             row += "\t" + str(matrix[i][j])
         print(row)
 
+def true_positives(predicted, observed, cls):
+    return np.sum(np.logical_and((predicted == cls), (observed == cls)))
+
+def true_negatives(predicted, observed, cls):
+    return np.sum(np.logical_and((predicted != cls), (observed != cls)))
+
+def false_positives(predicted, observed, cls):
+    return np.sum(np.logical_and((predicted == cls), (observed != cls)))
+
+def false_negatives(predicted, observed, cls):
+    return np.sum(np.logical_and((predicted != cls), (observed == cls)))
+
+def table_of_confusion(predicted, observed, cls):
+    classes = [cls, "not " + cls]
+    table = OrderedDict({})
+    for i in classes:
+        table[i] = OrderedDict(((cls2, 0) for cls2 in classes))
+    table[cls][cls] = true_positives(predicted, observed, cls)
+    table[cls]["not " + cls] = false_positives(predicted, observed, cls)
+    table["not " + cls][cls] = false_negatives(predicted, observed, cls)
+    table["not " + cls]["not " + cls] = true_negatives(predicted, observed, cls)
+    return table
+
 def precision(predicted, observed, cls):
-    true_positives = np.sum(np.logical_and((predicted == cls),
-                                           (observed == cls)
-                                          )
-                           )
-    return true_positives/len(observed)
+    tp = true_positives(predicted, observed, cls)
+    fp = false_positives(predicted, observed, cls)
+    return tp/(tp + fp)
 
 def recall(predicted, observed, cls):
-    true_positives = np.sum(np.logical_and((predicted == cls),
-                                           (observed == cls)
-                                          )
-                           )
-    false_negatives =
-    return true_positives/(true_positives + false_negatives)
+    tp = true_positives(predicted, observed, cls)
+    fn = false_negatives(predicted, observed, cls)
+    return tp/(tp + fn)
 
 def f1(predicted, observed, cls):
     prec = precision(predicted, observed, cls)
     rec = recall(predicted, observed, cls)
-    return 2*((prec*rec)/(prec+rec))
+    return 2 * ((prec * rec) / (prec + rec))
 
 # observed = np.array(("cat", "cat", "cat", "cat", "dog", "dog", "dog", "human", "human", "donkey"))
-# predicted = np.array(("dog", "cat", "cat", "dog", "donkey", "human", "human", "human", "donkey", "dog"))
+# predicted = np.array(("dog", "cat", "cat", "dog", "donkey", "cat", "human", "human", "donkey", "dog"))
 # mat = confusion_matrix(predicted, observed)
+# table = table_of_confusion(predicted, observed, "cat")
 # print_confusion_matrix(mat)
+# print_confusion_matrix(table)
 # print(list(enumerate(list(observed))))
 # print(list(enumerate(list(predicted))))
+# print("p: %0.2f" % precision(predicted, observed, "cat"))
+# print("r: %0.2f" % recall(predicted, observed, "cat"))
+# print("f1: %0.2f" % f1(predicted, observed, "cat"))
