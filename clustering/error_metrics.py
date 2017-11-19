2ec965c4675adb5b3b91e2380a7cc21d0ec543f3
Arturo CURIEL <curieldaz@gmail.com>
2017-11-18 21:17:18 -0300

    Put a general error metric

--- clustering/error_metrics.py
+++ clustering/error_metrics.py
@@ -89,83 +89,98 @@ def total_entropy(predicted, observed):
         total_entropy += h_w * (total_cluster_elements/total_elements)
     return total_entropy
 
-def true_positives(predicted, observed, cls):
-    return np.sum(np.logical_and((predicted == cls), (observed == cls)))
+# standard f1
 
-def true_negatives(predicted, observed, cls):
-    return np.sum(np.logical_and((predicted != cls), (observed != cls)))
+def true_positives_class(predicted, observed, cls):
+    return 1.0 * np.sum(np.logical_and((predicted == cls), (observed == cls)))
 
-def false_positives(predicted, observed, cls):
-    return np.sum(np.logical_and((predicted == cls), (observed != cls)))
+def true_negatives_class(predicted, observed, cls):
+    return 1.0 * np.sum(np.logical_and((predicted != cls), (observed != cls)))
 
-def false_negatives(predicted, observed, cls):
-    return np.sum(np.logical_and((predicted != cls), (observed == cls)))
+def false_positives_class(predicted, observed, cls):
+    return 1.0 * np.sum(np.logical_and((predicted == cls), (observed != cls)))
 
-def table_of_confusion(predicted, observed, cls):
+def false_negatives_class(predicted, observed, cls):
+    return 1.0 * np.sum(np.logical_and((predicted != cls), (observed == cls)))
+
+def table_of_confusion_class(predicted, observed, cls):
     classes = [cls, "not " + cls]
     table = OrderedDict({})
     for i in classes:
         table[i] = OrderedDict(((cls2, 0) for cls2 in classes))
-    table[cls][cls] = true_positives(predicted, observed, cls)
-    table[cls]["not " + cls] = false_positives(predicted, observed, cls)
-    table["not " + cls][cls] = false_negatives(predicted, observed, cls)
-    table["not " + cls]["not " + cls] = true_negatives(predicted, observed, cls)
+    table[cls][cls] = true_positives_class(predicted, observed, cls)
+    table[cls]["not " + cls] = false_positives_class(predicted, observed, cls)
+    table["not " + cls][cls] = false_negatives_class(predicted, observed, cls)
+    table["not " + cls]["not " + cls] = true_negatives_class(predicted, observed, cls)
     return table
 
-def precision(predicted, observed, cls):
-    tp = true_positives(predicted, observed, cls)
-    fp = false_positives(predicted, observed, cls)
+def precision_class(predicted, observed, cls):
+    tp = true_positives_class(predicted, observed, cls)
+    fp = false_positives_class(predicted, observed, cls)
     return tp/(tp + fp)
 
-def recall(predicted, observed, cls):
-    tp = true_positives(predicted, observed, cls)
-    fn = false_negatives(predicted, observed, cls)
+def recall_class(predicted, observed, cls):
+    tp = true_positives_class(predicted, observed, cls)
+    fn = false_negatives_class(predicted, observed, cls)
     return tp/(tp + fn)
 
-def f1(predicted, observed, cls):
-    prec = precision(predicted, observed, cls)
-    rec = recall(predicted, observed, cls)
+def f1_class(predicted, observed, cls):
+    prec = precision_class(predicted, observed, cls)
+    rec = recall_class(predicted, observed, cls)
+    if prec * rec == 0.0:
+        return 0.0
     return 2 * ((prec * rec) / (prec + rec))
+
+# general f1
+def general_f1(predicted, observed):
+    classes = set(observed.flatten())
+    total_f1 = 0.0
+    for cls in classes:
+        total_f1 += f1_class(predicted, observed, cls)
+    return total_f1/len(classes)
 
 # observed = np.array(("cat", "cat", "cat", "cat", "dog", "dog", "dog", "human", "human", "donkey"))
 # predicted = np.array(("dog", "cat", "cat", "dog", "donkey", "cat", "human", "human", "donkey", "dog"))
 # mat = confusion_matrix(predicted, observed)
 # print(purity(predicted, observed))
-# table = table_of_confusion(predicted, observed, "cat")
+# table = table_of_confusion_class(predicted, observed, "cat")
 # print_confusion_matrix(mat)
 # print_confusion_matrix(table)
 # print(list(enumerate(list(observed))))
 # print(list(enumerate(list(predicted))))
-# print("p: %0.2f" % precision(predicted, observed, "cat"))
-# print("r: %0.2f" % recall(predicted, observed, "cat"))
-# print("f1: %0.2f" % f1(predicted, observed, "cat"))
-
-observed = np.array(("x", "x", "x", "x", "x", "x", "x", "x",
-                     "o", "o", "o", "o", "o",
-                     "d", "d", "d", "d"))
-predicted = np.array(("x", "x", "x", "x", "x", "o", "d", "d",
-                      "x", "o", "o", "o", "o",
-                      "o", "d", "d", "d"))
-predicted2 = np.array(("x", "x", "x", "x", "x", "x", "x", "x",
-                       "o", "o", "o", "o", "o",
-                       "d", "d", "d", "d"))
-predicted3 = np.array(("o", "o", "o", "o", "o", "d", "d", "d",
-                       "x", "x", "x", "x", "d",
-                       "x", "x", "x", "x"))
-mat = confusion_matrix(predicted, observed)
-print_confusion_matrix(mat)
-print(purity(predicted, observed))
-print(purity_corrected(predicted, observed))
-print(total_entropy(predicted, observed))
-print()
-mat = confusion_matrix(predicted2, observed)
-print_confusion_matrix(mat)
-print(purity(predicted2, observed))
-print(purity_corrected(predicted2, observed))
-print(total_entropy(predicted2, observed))
-print()
-mat = confusion_matrix(predicted3, observed)
-print_confusion_matrix(mat)
-print(purity(predicted3, observed))
-print(purity_corrected(predicted3, observed))
-print(total_entropy(predicted3, observed))
+# print("p: %0.2f" % precision_class(predicted, observed, "cat"))
+# print("r: %0.2f" % recall_class(predicted, observed, "cat"))
+# print("f1: %0.2f" % f1_class(predicted, observed, "cat"))
+# 
+# observed = np.array(("x", "x", "x", "x", "x", "x", "x", "x",
+#                      "o", "o", "o", "o", "o",
+#                      "d", "d", "d", "d"))
+# predicted = np.array(("x", "x", "x", "x", "x", "o", "d", "d",
+#                       "x", "o", "o", "o", "o",
+#                       "o", "d", "d", "d"))
+# predicted2 = np.array(("x", "x", "x", "x", "x", "x", "x", "x",
+#                        "o", "o", "o", "o", "o",
+#                        "d", "d", "d", "d"))
+# predicted3 = np.array(("o", "o", "o", "o", "o", "d", "d", "d",
+#                        "x", "x", "x", "x", "d",
+#                        "x", "x", "x", "x"))
+# mat = confusion_matrix(predicted, observed)
+# print_confusion_matrix(mat)
+# print(purity(predicted, observed))
+# print(purity_corrected(predicted, observed))
+# print(total_entropy(predicted, observed))
+# print(general_f1(predicted, observed))
+# print()
+# mat = confusion_matrix(predicted2, observed)
+# print_confusion_matrix(mat)
+# print(purity(predicted2, observed))
+# print(purity_corrected(predicted2, observed))
+# print(total_entropy(predicted2, observed))
+# print(general_f1(predicted2, observed))
+# print()
+# mat = confusion_matrix(predicted3, observed)
+# print_confusion_matrix(mat)
+# print(purity(predicted3, observed))
+# print(purity_corrected(predicted3, observed))
+# print(total_entropy(predicted3, observed))
+# print(general_f1(predicted3, observed))
