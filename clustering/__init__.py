8644eef592e5d907d9a08a2de4b6b1976c6aeead
Arturo CURIEL <curieldaz@gmail.com>
2017-11-20 01:30:22 -0300

    Calculating baseline values

--- clustering/__init__.py
+++ clustering/__init__.py
@@ -26,22 +26,22 @@ init_centroids = processing.get_initial_centroids(vect_docs, ideal_centroids)
 base_init, means_init, tags_init, expected_init = kmeans.kmeans(
                                                      vect_docs,
                                                      init_centroids)
-print("init f1 : %s" % err.general_f1(base_init, expected_init))
 print("init purity : %s" % err.purity(base_init, expected_init))
 print("init purity corr : %s" % err.purity_corrected(base_init, expected_init))
+print("init f1 : %s" % err.general_f1(base_init, expected_init))
 print("init entropy : %s" % err.total_entropy(base_init, expected_init))
 
 base_ideal, means_ideal, tags_ideal, expected_ideal = kmeans.kmeans(
                                     vect_docs,
                                     np.array(list(ideal_centroids.values())))
-print("ideal f1 : %s" % err.general_f1(base_ideal, expected_ideal))
 print("ideal purity : %s" % err.purity(base_ideal, expected_ideal))
 print("ideal purity corr : %s" % err.purity_corrected(base_ideal,
                                                       expected_ideal))
+print("ideal f1 : %s" % err.general_f1(base_ideal, expected_ideal))
 print("ideal entropy : %s" % err.total_entropy(base_ideal, expected_ideal))
 
-del vect_docs
 del docs
+del vect_docs
 
 docs = duc.get_rouge_summary_clusters(data_folder_summ10)
 vect_docs = duc.convert_to_vectors(docs, space)
@@ -51,22 +51,22 @@ print("\nSum10")
 summ10_init, means_init, tags_init, expected_init = kmeans.kmeans(
                                                                 vect_docs,
                                                                 init_centroids)
-print("init f1 : %s" % err.general_f1(summ10_init, expected_init))
 print("init purity : %s" % err.purity(summ10_init, expected_init))
 print("init purity corr : %s" % err.purity_corrected(summ10_init, expected_init))
+print("init f1 : %s" % err.general_f1(summ10_init, expected_init))
 print("init entropy : %s" % err.total_entropy(summ10_init, expected_init))
 
 summ10_ideal, means_ideal, tags_ideal, expected_ideal = kmeans.kmeans(
                                     vect_docs,
                                     np.array(list(ideal_centroids.values())))
-print("ideal f1 : %s" % err.general_f1(summ10_ideal, expected_ideal))
 print("ideal purity : %s" % err.purity(summ10_ideal, expected_ideal))
 print("ideal purity corr : %s" % err.purity_corrected(summ10_ideal,
                                                       expected_ideal))
+print("ideal f1 : %s" % err.general_f1(summ10_ideal, expected_ideal))
 print("ideal entropy : %s" % err.total_entropy(summ10_ideal, expected_ideal))
 
-del vect_docs
 del docs
+del vect_docs
 
 docs = duc.get_rouge_summary_clusters(data_folder_summ100)
 vect_docs = duc.convert_to_vectors(docs, space)
@@ -76,16 +76,16 @@ print("\nSum100")
 summ100_init, means_init, tags_init, expected_init = kmeans.kmeans(
                                                                 vect_docs,
                                                                 init_centroids)
-print("init f1 : %s" % err.general_f1(summ100_init, expected_init))
 print("init purity : %s" % err.purity(summ100_init, expected_init))
 print("init purity corr : %s" % err.purity_corrected(summ100_init, expected_init))
+print("init f1 : %s" % err.general_f1(summ100_init, expected_init))
 print("init entropy : %s" % err.total_entropy(summ100_init, expected_init))
 
 summ100_ideal, means_ideal, tags_ideal, expected_ideal = kmeans.kmeans(
                                     vect_docs,
                                     np.array(list(ideal_centroids.values())))
-print("ideal f1 : %s" % err.general_f1(summ100_ideal, expected_ideal))
 print("ideal purity : %s" % err.purity(summ100_ideal, expected_ideal))
 print("ideal purity corr : %s" % err.purity_corrected(summ100_ideal,
                                                       expected_ideal))
+print("ideal f1 : %s" % err.general_f1(summ100_ideal, expected_ideal))
 print("ideal entropy : %s" % err.total_entropy(summ100_ideal, expected_ideal))
