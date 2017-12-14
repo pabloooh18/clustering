e968b44a30cfadded279fa7070359676b109b2d0
pabloooh18 <pansoto@alumnos.ubiobio.cl>
2017-12-14 17:20:28 -0300

    modificacion de kmeans

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -18,14 +18,15 @@ k_means_.euclidean_distances = new_multiply
 
 
 
-def kmeans(duc_vectorized_documents, means):
+def kmeans(duc_vectorized_documents, means=np.array([])):
+
     tags = []
     y = []
-    x = []       
+    x = [] 
+    resultado=[]      
     #means=means.tolist() #convierte en una lista pero solo una.
-    means=[list(f) for f in means] #con list() queda como una matris pero con el formato array([])  
+    #means=[list(f) for f in means] #con list() queda como una matris pero con el formato array([])  
     label = 0
-
     for cluster in duc_vectorized_documents:
         for document in duc_vectorized_documents[cluster]:
             y.append(label)
@@ -35,17 +36,29 @@ def kmeans(duc_vectorized_documents, means):
             else:
                 x = np.vstack([x, duc_vectorized_documents[cluster][document]])
         label += 1
-    #if len(means)>0:
+    
+    for vector in x:
+        best_distance = best_index = None
+        for index in range(len(means)):
+            mean = means[index]            
+            dist = util.euclidean_distance(vector, mean)
+            if best_distance is None or dist < best_distance:
+                best_index, best_distance = index, dist
+        resultado.append(best_index)        
+        #import ipdb
+        #ipdb.set_trace()            
+           
+    # #if len(means)>0:
     #import ipdb
     #ipdb.set_trace()
-    x=[np.array(f) for f in x]
+    # x=[np.array(f) for f in x]
             
-    kmean = KMeansClusterer(label, distance=euclidean_distance,initial_means=means,avoid_empty_clusters=True)
-    kmeans = kmean.cluster(x,True)
-        # KMeans(n_clusters=label,init=means,algorithm="full").fit(x)
-    #else:    
-      # kmean = KMeansClusterer(label, distance=euclidean_distance)
-       #kmeans = kmean.cluster(x)
-        #KMeans(n_clusters=label,algorithm="full").fit(x)
+    # kmean = KMeansClusterer(label, distance=euclidean_distance,initial_means=None,avoid_empty_clusters=True)
+    # kmeans = kmean.cluster(x,assign_clusters = True)
+    #     # KMeans(n_clusters=label,init=means,algorithm="full").fit(x)
+    # #else:    
+    #   # kmean = KMeansClusterer(label, distance=euclidean_distance)
+    #    #kmeans = kmean.cluster(x)
+    #     #KMeans(n_clusters=label,algorithm="full").fit(x)
 
-    return kmeans, kmean.means(), np.array(tags), np.array(y)
+    return resultado, means, np.array(tags), np.array(y)
