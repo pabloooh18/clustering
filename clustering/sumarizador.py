6b0749de1b7eb9f87427617626af5dd662b49ed3
pabloooh18 <pansoto@alumnos.ubiobio.cl>
2018-11-25 14:44:06 -0300

    acortador de palabras

--- clustering/sumarizador.py
+++ clustering/sumarizador.py
@@ -13,7 +13,22 @@ from string import punctuation
 sys.setrecursionlimit(5000)
 data_folder_original = "../res/original"
 
-
+def summary_wordlimit(summary_sentences, sentence_scores, word_limit):
+    limited_summary = []
+    current_length = 0
+    score_position = [(i,score) for i, score in enumerate(sentence_scores)]
+    score_position.sort(key=lambda x:x[1], reverse=True)
+    #import ipdb;ipdb.set_trace()
+    for position, score in score_position:
+        raw_sent= summary_sentences[position]
+        if (current_length + raw_sent.count(" ") + 1) <= word_limit:
+            limited_summary.append(position)
+            current_length += raw_sent.count(" ") + 1
+    limited_summary.sort()
+    limited_summary = [summary_sentences[pos] for pos in limited_summary]
+    #import ipdb;ipdb.set_trace()
+    return limited_summary
+			
 
 cluster_ids = os.listdir(data_folder_original)
 documents = {}
@@ -25,9 +40,12 @@ score_doc1=[]
 score_doc2=[]
 sentence_list=[]
 result_lcs=[]
+summary_sentence_scores=[]
 resume=""
-resume_printf=""
+resume_printf=[]
+new_resume=""
 count_printf=0
+
 #import ipdb;ipdb.set_trace()
 # Gets the tokens
 for cluster in cluster_ids:
@@ -63,7 +81,12 @@ for cluster in cluster_ids:
 			#import ipdb;ipdb.set_trace() 
 			if var==2:
 				first_document=resume
-				first_original_sentence_list = sent_tokenize(resume_printf)
+				#import ipdb;ipdb.set_trace()
+				
+				new_resume=" ".join(resume_printf)
+
+				#import ipdb;ipdb.set_trace()	
+				first_original_sentence_list = sent_tokenize(new_resume)
 				for sentence in first_original_sentence_list: #quita puntuacion y stopwords de cada oracion			
 					sentence = ' '.join([word for word in word_tokenize(sentence.translate(table))
 				  						if word not in en_stopwords])
@@ -72,7 +95,8 @@ for cluster in cluster_ids:
 				tokenized_phrase1 = first_document.split(" ")
 				tokenized_phrase2 = tokens.split(" ")
 				resume=""
-				resume_printf=""
+				resume_printf=[]
+				summary_sentence_scores=[]
 
 			else:
 				var=2	
@@ -106,37 +130,79 @@ for cluster in cluster_ids:
 								flag=1
 						if flag==1:
 							resume= resume + first_sentence_list[count]
-							resume_printf = resume_printf + first_original_sentence_list[count]
+							resume_printf.append(first_original_sentence_list[count])
+							summary_sentence_scores.append(score_doc1)
 							flag=0	
 						first_original_sentence_list.pop(count)		
 						first_sentence_list.pop(count)
-					else:												
-						for elem in contains_words3:
-							if elem in result_lcs:
-								result_lcs.remove(elem)
-								flag=1
-						if flag==1:
-							resume= resume + sentence_list[count]
-							resume_printf = resume_printf + sent_tokenize_list[count]
-							flag=0
+						
+					else:
+						if score_doc1 == score_doc2:	
+							if sentence_list:
+								for elem in contains_words3:
+									if elem in result_lcs:
+										result_lcs.remove(elem)
+										flag=1
+								if flag==1:
+									resume= resume + sentence_list[count]
+									resume_printf.append(sent_tokenize_list[count])
+									summary_sentence_scores.append(score_doc2)
+									flag=0
 
-						sentence_list.pop(count)
-						sent_tokenize_list.pop(count)			
+								sentence_list.pop(count)
+								sent_tokenize_list.pop(count)
+								
+							else:	
+								if first_sentence_list:
+									for elem in contains_words1:
+										if elem in result_lcs:	
+											result_lcs.remove(elem)
+											flag=1
+									if flag==1:
+										resume= resume + first_sentence_list[count]
+										resume_printf.append(first_original_sentence_list[count])
+										summary_sentence_scores.append(score_doc1)
+										flag=0	
+									first_original_sentence_list.pop(count)		
+									first_sentence_list.pop(count)
+										
+
+
+
+						else:													
+							for elem in contains_words3:
+								if elem in result_lcs:
+									result_lcs.remove(elem)
+									flag=1
+							if flag==1:
+								resume= resume + sentence_list[count]
+								resume_printf.append(sent_tokenize_list[count])
+								summary_sentence_scores.append(score_doc2)	
+								flag=0
+
+							sentence_list.pop(count)
+							sent_tokenize_list.pop(count)
+									
 
 						
 					#import ipdb;ipdb.set_trace()
 					contains_words1=[]
-					contains_words3=[]
+					contains_words3=[]					
 					score_doc1=0
 					score_doc2=0
 
 			count_printf+=1	
 			if count_printf==9:
-				print("Resumen: ", resume_printf)
+				for_printf=""
+				for_printf=" ".join(summary_wordlimit(resume_printf,summary_sentence_scores,15))
+				print("\n\n\nResumen de "+ cluster +": " +for_printf)
 				count_printf=0	
 			first_sentence_list=[]
 			sentence_list=[]
-			
+
+
+
+
 
 
 
