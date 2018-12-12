99f230e95bdf935042e21515c7e722e394472e47
pabloooh18 <pansoto@alumnos.ubiobio.cl>
2018-12-12 08:00:03 -0300

    imprimir en carpetas correcciones varias

--- clustering/sumarizador.py
+++ clustering/sumarizador.py
@@ -16,44 +16,37 @@ import pandas as pd
 sys.setrecursionlimit(5000)
 data_folder_original = "../res/original"
 
-def flatten(multidim_list):
-    if isinstance(multidim_list, list):
-        if len(multidim_list) > 1:
-            return flatten(multidim_list[0]) + flatten(multidim_list[1:])
-        elif len(multidim_list) > 0:
-            return flatten(multidim_list[0])
-        else:
-            return []
-    else:
-        return [multidim_list]
-
 def summary_wordlimit(summary_sentences, sentence_scores, word_limit):
-    limited_summary = []
-    current_length = 0
-    score_position = [(i,score) for i, score in enumerate(sentence_scores)]
-    score_position.sort(key=lambda x:x[1], reverse=True)
-    #import ipdb;ipdb.set_trace()
-    for position, score in score_position:
-        raw_sent= summary_sentences[position]
-        if (current_length + raw_sent.count(" ") + 1) <= word_limit:
-            limited_summary.append(position)
-            current_length += raw_sent.count(" ") + 1
-    limited_summary.sort()
-    limited_summary = [summary_sentences[pos] for pos in limited_summary]
-    #import ipdb;ipdb.set_trace()
-    return limited_summary
-			
-def calculate_idf(tokenized_documents):
-    type_set = list(set(flatten(tokenized_documents)))
-    idf = {}
-    total_num_of_docs = len(tokenized_documents)
-    for word in type_set:
-        idf[word] = 0
-        for document in tokenized_documents:
-            if word in document:
-                idf[word] += 1
-        idf[word] = log(total_num_of_docs / idf[word])
-    return idf
+	limited_summary = []
+	current_length = 0
+	score_position = [(i,score) for i, score in enumerate(sentence_scores)]
+	score_position.sort(key=lambda x:x[1], reverse=True)
+	#import ipdb;ipdb.set_trace()
+	for position, score in score_position:
+		raw_sent= summary_sentences[position]
+		if (current_length + raw_sent.count(" ") + 1) <= word_limit:
+			limited_summary.append(position)
+			current_length += raw_sent.count(" ") + 1
+	limited_summary.sort()
+	limited_summary = [summary_sentences[pos] for pos in limited_summary]
+	#import ipdb;ipdb.set_trace()
+	return limited_summary
+
+def summary_limit(summary_sentences, sentence_scores, byte_limit):
+	limited_summary = []
+	current_length = 0
+	score_position = [(i,score) for i, score in enumerate(sentence_scores)]
+	score_position.sort(key=lambda x:x[1], reverse=True)
+
+	for position, score in score_position:
+		raw_sent= summary_sentences[position]
+		if (current_length + len(raw_sent.encode("utf-8"))) <= byte_limit:
+			limited_summary.append(position)
+			current_length += len(raw_sent.encode("utf-8"))
+	limited_summary.sort()
+	limited_summary = [summary_sentences[pos] for pos in limited_summary]
+	return limited_summary    
+
 
 cluster_ids = os.listdir(data_folder_original)
 documents = {}
@@ -75,11 +68,13 @@ count_printf=0
 # Gets the tokens
 for cluster in cluster_ids:
 	
+	
 	cluster_folder = data_folder_original + "/" + cluster
 	documents[cluster] = {}
 	var=0
 
-	for document in os.listdir(cluster_folder):				
+	for document in os.listdir(cluster_folder):	
+		print(cluster)			
 		document_path = cluster_folder + "/" + document
 		tree = ET.parse(document_path)
 		root = tree.getroot()
@@ -93,7 +88,7 @@ for cluster in cluster_ids:
 		#import ipdb;ipdb.set_trace()
 		for sentence in sent_tokenize_list: #quita puntuacion y stopwords de cada oracion			
 			sentence = ' '.join([word for word in word_tokenize(sentence.translate(table))
-			  						if word not in en_stopwords])
+									if word not in en_stopwords])
 			sentence_list.append(sentence)  						 
 		#import ipdb;ipdb.set_trace()	      
 		if var==0:
@@ -114,7 +109,7 @@ for cluster in cluster_ids:
 				first_original_sentence_list = sent_tokenize(new_resume)
 				for sentence in first_original_sentence_list: #quita puntuacion y stopwords de cada oracion			
 					sentence = ' '.join([word for word in word_tokenize(sentence.translate(table))
-				  						if word not in en_stopwords])
+										if word not in en_stopwords])
 					first_sentence_list.append(sentence)
 				#import ipdb;ipdb.set_trace()	
 				tokenized_phrase1 = first_document.split(" ")
@@ -156,8 +151,8 @@ for cluster in cluster_ids:
 						#import ipdb;ipdb.set_trace()
 					if sentence_list:
 						contains_words3=lcs(result_lcs,sentence_list[count].split(" "), " ").split(" ")	
-						# if '' in contains_words3:
-						# 	contains_words3.remove('')
+						if '' in contains_words3:
+							contains_words3.remove('')
 						# if contains_words3:	
 						# 	tfidf=TfidfVectorizer(vocabulary=sorted(set(contains_words3)),ngram_range=(1,2))
 						# 	tfs=tfidf.fit_transform(sentence_list[count].split(" "))
@@ -210,10 +205,6 @@ for cluster in cluster_ids:
 										flag=0	
 									first_original_sentence_list.pop(count)		
 									first_sentence_list.pop(count)
-										
-
-
-
 						else:													
 							for elem in contains_words3:
 								if elem in result_lcs:
@@ -236,12 +227,40 @@ for cluster in cluster_ids:
 					score_doc1=0
 					score_doc2=0
 
+			
+			for_printf=""
+			#summary_limit limite por byte 665 ~100 palabras
+			#summary_wordlimit por palabra
+			for_printf=" ".join(summary_limit(resume_printf,summary_sentence_scores,665))
+			
+			# para imprimir en carpeta
+
+			# folder_path = "../summaries/%s" % (cluster)
+			# try:
+			# 	os.makedirs(folder_path)
+			# except:
+			# 	pass
+			# with open("%s/%s_%s.txt" % (folder_path, document, count_printf), "w") as f: f.write(for_printf)
+
+			print("\n\n\nResumen de "+str(count_printf)+"--" + document +": " +for_printf)
 			count_printf+=1	
 			if count_printf==9:
-				for_printf=""
-				for_printf=" ".join(summary_wordlimit(resume_printf,summary_sentence_scores,100))
-				print("\n\n\nResumen de "+ cluster +": " +for_printf)
-				count_printf=0	
+				#import ipdb;ipdb.set_trace()
+				contains_words1=[]
+				contains_words2=[]
+				contains_words3=[]
+				contains_words4=[]
+				score_doc1=0
+				score_doc2=0
+				sentence_list=[]
+				result_lcs=[]
+				summary_sentence_scores=[]
+				resume=""
+				resume_printf=[]
+				new_resume=""
+				count_printf=0
+				var=0
+
 			first_sentence_list=[]
 			sentence_list=[]
 
--- clustering/sumarizador_spanish.py
+++ clustering/sumarizador_spanish.py
@@ -16,22 +16,55 @@ from processing import *
 from lcs import *
 from freeling import *
 from string import punctuation
+from sklearn.feature_extraction.text import TfidfVectorizer
 es_sent_tokenize = nltk.data.load('tokenizers/punkt/spanish.pickle')
 def freeling_preprocess(raw_doc):
-    '''returns a list of sentences with their tokenizations'''
-    client = FreelingClient('localhost', 5005)
-    results = []
-    all_tokens = []
-    sentences = es_sent_tokenize.tokenize(raw_doc)
-    for sentence in sentences:
-        tokenized = client.sent_process(sentence)
-        results.append(tokenized)
-        all_tokens += tokenized
-    return results, all_tokens
+	'''returns a list of sentences with their tokenizations'''
+	client = FreelingClient('localhost', 5005)
+	results = []
+	all_tokens = []
+	sentences = es_sent_tokenize.tokenize(raw_doc)
+	for sentence in sentences:
+		tokenized = client.sent_process(sentence)
+		results.append(tokenized)
+		all_tokens += tokenized
+	return results, all_tokens
 locale.setlocale(locale.LC_ALL, 'es_ES.utf8')
 sys.setrecursionlimit(10000)
-data_folder_original = "../spanish"
+# data_folder_original = "../spanish"
+data_folder_original = "../esoriginal"
 
+
+def summary_wordlimit(summary_sentences, sentence_scores, word_limit):
+	limited_summary = []
+	current_length = 0
+	score_position = [(i,score) for i, score in enumerate(sentence_scores)]
+	score_position.sort(key=lambda x:x[1], reverse=True)
+	#import ipdb;ipdb.set_trace()
+	for position, score in score_position:
+		raw_sent= summary_sentences[position]
+		if (current_length + raw_sent.count(" ") + 1) <= word_limit:
+			limited_summary.append(position)
+			current_length += raw_sent.count(" ") + 1
+	limited_summary.sort()
+	limited_summary = [summary_sentences[pos] for pos in limited_summary]
+	#import ipdb;ipdb.set_trace()
+	return limited_summary
+
+def summary_limit(summary_sentences, sentence_scores, byte_limit):
+	limited_summary = []
+	current_length = 0
+	score_position = [(i,score) for i, score in enumerate(sentence_scores)]
+	score_position.sort(key=lambda x:x[1], reverse=True)
+
+	for position, score in score_position:
+		raw_sent= summary_sentences[position]
+		if (current_length + len(raw_sent.encode("utf-8"))) <= byte_limit:
+			limited_summary.append(position)
+			current_length += len(raw_sent.encode("utf-8"))
+	limited_summary.sort()
+	limited_summary = [summary_sentences[pos] for pos in limited_summary]
+	return limited_summary 
 
 
 cluster_ids = os.listdir(data_folder_original)
@@ -40,29 +73,39 @@ contains_words1=[]
 contains_words2=[]
 contains_words3=[]
 contains_words4=[]
-score_doc1=[]
-score_doc2=[]
+score_doc1=0
+score_doc2=0
 sentence_list=[]
 result_lcs=[]
+summary_sentence_scores=[]
 resume=""
-resume_printf=""
+resume_printf=[]
+new_resume=""
 count_printf=0
 #import ipdb;ipdb.set_trace()
 # Gets the tokens
 for cluster in cluster_ids:
 	
 	cluster_folder = data_folder_original + "/" + cluster
-	documents[cluster] = {}
 	var=0
 
-	for document in os.listdir(cluster_folder):				
-		document_path = cluster_folder + "/" + document		
-		original_text = open(document_path, encoding='utf-8').read()
-		original_text = unicodedata.normalize("NFKD", original_text)
-		original_text = original_text.replace(u'\ufeff', u' ')
-		original_text = original_text.replace(u'\n', u' ')	
-		# original_text.encode('raw_unicode_escape').decode('utf-8')			
-		table = str.maketrans(dict.fromkeys(punctuation))
+	document_path = cluster_folder + "/" + os.listdir(cluster_folder)[0]
+	print(document_path)
+	tree = ET.parse(document_path)
+	root = tree.getroot()
+	for i, news_item in enumerate(root):
+		#print(i+1, ": ", news_item.find("title").text)
+		original_text = news_item.find("content").text
+	# cluster_folder = data_folder_original + "/" + cluster
+	# documents[cluster] = {}	
+	# for document in os.listdir(cluster_folder):				
+	# 	document_path = cluster_folder + "/" + document		
+	# 	original_text = open(document_path, encoding='utf-8').read()
+	# 	original_text = unicodedata.normalize("NFKD", original_text)
+	# 	original_text = original_text.replace(u'\ufeff', u' ')
+	# 	original_text = original_text.replace(u'\n', u' ')	
+	# 	# original_text.encode('raw_unicode_escape').decode('utf-8')			
+	# 	table = str.maketrans(dict.fromkeys(punctuation))
 		sent_tokenize_list = es_sent_tokenize.tokenize(original_text)			
 		sentence_list, tokens = freeling_preprocess(original_text)
 		#import ipdb;ipdb.set_trace()								 
@@ -76,14 +119,16 @@ for cluster in cluster_ids:
 		else:
 			#import ipdb;ipdb.set_trace() 
 			if var==2:
-				first_document=resume_printf
-				first_original_sentence_list = es_sent_tokenize.tokenize(resume_printf)					
+				# import ipdb;ipdb.set_trace()
+				first_document=" ".join(resume_printf)
+				first_original_sentence_list = es_sent_tokenize.tokenize(" ".join(resume_printf))					
 				first_sentence_list, tokens_resume = freeling_preprocess(first_document)	
 				#import ipdb;ipdb.set_trace()	
-				tokenized_phrase1 = resume_printf.split(" ")
+				tokenized_phrase1 = first_document.split(" ")
 				tokenized_phrase2 = tokens
 				resume=""
-				resume_printf=""
+				resume_printf=[]
+				summary_sentence_scores=[]
 
 			else:
 				var=2	
@@ -104,11 +149,28 @@ for cluster in cluster_ids:
 						contains_words1=lcs(result_lcs,first_sentence_list[count]," ").split(" ")
 						if '' in contains_words1:
 							contains_words1.remove('')
+						#import ipdb;ipdb.set_trace()
+						# if contains_words1:	
+						# 	tfidf=TfidfVectorizer(vocabulary=sorted(set(contains_words1)),ngram_range=(1,2))
+						# 	tfs=tfidf.fit_transform(first_sentence_list[count])
+						# 	rows, cols = tfs.nonzero()
+						# 	for row, col in zip(rows, cols):
+						# 		score_doc1=score_doc1+tfs[row,col]
+						# else:
+						# 	score_doc1=0	
 						score_doc1=len(contains_words1)/len(first_sentence_list[count])	
 					if sentence_list:
 						contains_words3=lcs(result_lcs,sentence_list[count], " ").split(" ")	
 						if '' in contains_words3:
-							contains_words3.remove('')		
+							contains_words3.remove('')
+						# if contains_words3:	
+						# 	tfidf=TfidfVectorizer(vocabulary=sorted(set(contains_words3)),ngram_range=(1,2))
+						# 	tfs=tfidf.fit_transform(sentence_list[count])
+						# 	rows, cols = tfs.nonzero()
+						# 	for row, col in zip(rows, cols):
+						# 		score_doc2=score_doc2+tfs[row,col]
+						# else:
+						# 	score_doc2=0			
 						score_doc2=len(contains_words3)/len(sentence_list[count])
 					if score_doc1 > score_doc2:											
 						for elem in contains_words1:
@@ -117,36 +179,98 @@ for cluster in cluster_ids:
 								flag=1
 						if flag==1:
 							#resume= resume + first_sentence_list[count]
-							resume_printf = resume_printf + first_original_sentence_list[count]
+							resume_printf.append(first_original_sentence_list[count])
+							summary_sentence_scores.append(score_doc1)
 							flag=0
 															
 						first_original_sentence_list.pop(count)		
 						first_sentence_list.pop(count)
-					else:												
-						for elem in contains_words3:
-							if elem in result_lcs:
-								result_lcs.remove(elem)
-								flag=1
-						if flag==1:
-							#resume= resume + sentence_list[count]
-							resume_printf = resume_printf + sent_tokenize_list[count]
-							flag=0
-						
-						sentence_list.pop(count)
-						sent_tokenize_list.pop(count)			
+					else:
+						if score_doc1 == score_doc2:	
+							if sentence_list:
+								for elem in contains_words3:
+									if elem in result_lcs:
+										result_lcs.remove(elem)
+										flag=1
+								if flag==1:
+									# resume= resume + sentence_list[count]
+									resume_printf.append(sent_tokenize_list[count])
+									summary_sentence_scores.append(score_doc2)
+									flag=0
+
+								sentence_list.pop(count)
+								sent_tokenize_list.pop(count)
+								
+							else:	
+								if first_sentence_list:
+									for elem in contains_words1:
+										if elem in result_lcs:	
+											result_lcs.remove(elem)
+											flag=1
+									if flag==1:
+										# resume= resume + first_sentence_list[count]
+										resume_printf.append(first_original_sentence_list[count])
+										summary_sentence_scores.append(score_doc1)
+										flag=0	
+									first_original_sentence_list.pop(count)		
+									first_sentence_list.pop(count)
+						else:													
+							for elem in contains_words3:
+								if elem in result_lcs:
+									result_lcs.remove(elem)
+									flag=1
+							if flag==1:
+								#resume= resume + sentence_list[count]
+								resume_printf.append(sent_tokenize_list[count])
+								summary_sentence_scores.append(score_doc2)	
+								flag=0
+
+							sentence_list.pop(count)
+							sent_tokenize_list.pop(count)
+									
 
 						
 					#import ipdb;ipdb.set_trace()
 					contains_words1=[]
-					contains_words3=[]
+					contains_words3=[]					
 					score_doc1=0
 					score_doc2=0
+															
+						
 
-			count_printf+=1	
-			if count_printf==9:
-			# 	import ipdb;ipdb.set_trace()
-				print("Resumen: ", resume_printf)
-			#	count_printf=0	
+			# count_printf+=1	
+			# if count_printf==9:
+			# import ipdb;ipdb.set_trace()
+			for_printf=""
+			for_printf=" ".join(summary_wordlimit(resume_printf,summary_sentence_scores,100))
+
+			# para imprimir en carpeta
+
+			# folder_path = "../summaries/%s" % (cluster)
+			# try:
+			# 	os.makedirs(folder_path)
+			# except:
+			# 	pass
+			# with open("%s/%s_%s.txt" % (folder_path, i, count_printf), "w") as f: f.write(for_printf)
+			print("\n\nResumen: ", for_printf)
+				# count_printf=0
+			# count_printf+=1	
+			# if count_printf==9:
+			# 	#import ipdb;ipdb.set_trace()
+			# 	contains_words1=[]
+			# 	contains_words2=[]
+			# 	contains_words3=[]
+			# 	contains_words4=[]
+			# 	score_doc1=0
+			# 	score_doc2=0
+			# 	sentence_list=[]
+			# 	result_lcs=[]
+			# 	summary_sentence_scores=[]
+			# 	resume=""
+			# 	resume_printf=[]
+			# 	new_resume=""
+			# 	count_printf=0
+			# 	var=0		
 			first_sentence_list=[]
 			sentence_list=[]
 			
